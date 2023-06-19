dc771f0a4fc84fa6e212f1dd6098377c7de1f07c
Artem <artem.egorov.art2006@gmail.com>
2023-06-19 13:46:47 +0300

    Сделал показ карточки id+вывод Скиллов (если есть)

--- src/components/UI/button/ButtonGitHub.jsx
+++ src/components/UI/button/ButtonGitHub.jsx
@@ -5,8 +5,8 @@ import "../../../styles/main.css"
 export default function ButtonGitHub() {
   return (
     <a href="#!" className="btn-outline">
-    <img src={gitHubIcon} alt=""/>
-    GitHub repo
-</a>
+      <img src={gitHubIcon} alt=""/>
+      GitHub repo
+    </a>
   )
 }
