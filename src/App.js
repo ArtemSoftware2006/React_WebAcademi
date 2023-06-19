78ebccfecf0952b50a0b53173394d3f1bc49f217
Artem <artem.egorov.art2006@gmail.com>
2023-06-19 13:33:19 +0300

    Настроил при переходе на новую стр прокрутку вверх

--- src/App.js
+++ src/App.js
@@ -8,12 +8,14 @@ import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import Contacts from "./pages/Contacts";
 import Project from "../src/pages/Project";
+import ScrollToTop from "./utils/ScrollToTop.jsx";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
 
+        <ScrollToTop/>
         <Navbar/>
 
         <Routes>
